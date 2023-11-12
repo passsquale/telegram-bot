72d942f04a45bdcb007883dcf54edc4531e147d8
Evgeniy Roslyackov <eveniy.roslyackov@gmail.com>
2023-11-12 15:10:06 +0300

    create echo bot

--- cmd/telegram-bot/main.go
+++ cmd/telegram-bot/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func main() {
-	fmt.Println("Telegram bot started...")
+	godotenv.Load()
+
+	token := os.Getenv("TOKEN")
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Your text: "+update.Message.Text)
+
+			bot.Send(msg)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/passsquale/telegram-bot
 
 go 1.21
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
